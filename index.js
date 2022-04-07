bb38fbfe91a2a38154816569e663d5ab6b61908b
zhangyong <1046788379@qq.com>
2022-04-07 10:04:12 +0800

    修改日期

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /**
- * 移除中文
+ * 移除中文,与中文符号
  */
- export let removeChinese = (txt)=>{
-	return txt.replace(/[\u4E00-\u9FA5]|[\uFE30-\uFFA0]/g,'');
+export let removeChinese = (txt) => {
+	return txt.replace(/[\u4E00-\u9FA5]|[\uFE30-\uFFA0]/g, '');
 }
 
 /**
  * numberStr 19920817
  * return  年龄
  */
-export let calcuAge = (numberStr)=>{
+export let calcuAge = (numberStr) => {
 	let year = numberStr.slice(0, 4) // 获取生日
 	let month = numberStr.slice(4, 6) // 获取生日
 	let day = numberStr.slice(6, 8) // 获取生日
@@ -19,28 +19,30 @@ export let calcuAge = (numberStr)=>{
 	return age
 }
 // 是否包含中文字
-export let  hasChinese = (val)=>{
-	var reg = new RegExp("[\\u4E00-\\u9FFF]+","g");
+export let hasChinese = (val) => {
+	var reg = new RegExp("[\\u4E00-\\u9FFF]+", "g");
 	return reg.test(val)
 }
 
 // 判断是安卓，还是iOS
-export let getPhoneType=()=>{
-    var u = navigator.userAgent;
-    var isXiaomi = u.indexOf('XiaoMi') > -1; // 小米手机
-    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; // 其它安卓
-    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios
-    if (isAndroid || isXiaomi) {
-      return 'android';
-    }else if (isIOS) {
-      return 'ios';
-    }
+export let getPhoneType = () => {
+	var u = navigator.userAgent;
+	var isXiaomi = u.indexOf('XiaoMi') > -1; // 小米手机
+	var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; // 其它安卓
+	var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); // ios
+	if (isAndroid || isXiaomi) {
+		return 'android';
+	} else if (isIOS) {
+		return 'ios';
+	}
 }
-// 获取本地时间，2021-08-21 ,不足10，签名补充0
-export let getLocalDate=()=>{
-	let localTime = new Date().toLocaleDateString() // 2012/09/23
-	let localTimeArr = localTime.split('/')
-	localTimeArr[1] = localTimeArr[1]>=10?localTimeArr[1]:'0'+localTimeArr[1] 
-	localTimeArr[2] = localTimeArr[2]>=10?localTimeArr[2]:'0'+localTimeArr[2] 
-	return localTimeArr.join('-')
+// 获取日期时间，2021-08-21 ,不足10，签名补充0
+export let getLocalDate = (sign = '-') => {
+	let dateObj = new Date()
+	let year = dateObj.getFullYear()
+	let month = dateObj.getMonth() + 1
+	month = month <= 9 ? '0' + month : month
+	let date = dateObj.getDate()
+	date = date <= 9 ? '0' + date : date
+	return year + sign + month + sign + date
 }
